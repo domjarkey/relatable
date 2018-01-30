88cf050b6e33bfb24b43fb1b75c867880d6ee282
Dominic Jarkey <dominic.jarkey@gmail.com>
2018-01-29 23:58:52 -0600

    Include documentation for elements data set

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Data from the periodic table of elements
+#'
+#' A dataset containing atomic numbers, chemical symbols, and
+#' names of 118 elements.
+#'
+#' @format A data frame with 118 rows and 3 variables:
+#' \describe{
+#'   \item{Z}{Atomic number}
+#'   \item{Symbol}{Chemical symbol}
+#'   \item{Name}{Name of element}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Symbol_(chemistry)}
+"elements"
