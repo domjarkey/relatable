8259c340939fd176cd99b57b881634dae9deaa05
Dominic Jarkey <dominic.jarkey@gmail.com>
2018-01-31 15:48:21 -0600

    Add US_states dataset for state name abbreviations.

--- R/data.R
+++ R/data.R
@@ -11,3 +11,21 @@
 #' }
 #' @source \url{https://en.wikipedia.org/wiki/Symbol_(chemistry)}
 "elements"
+#' Data of US state names and abbreviations
+#'
+#' A dataset containing names of US states and their abbreviations under various standards
+#' including ANSI, ISO, USPS, USCG, GPO, Associated Press.
+#'
+#' @format A data frame with 51 rows and 10 variables:
+#' \describe{
+#'   \item{Name}{US Region Name}
+#'   \item{Type}{State, State (Commonwealth), or Federal District}
+#'   \item{ANSI}{American National Standards Institute 2-letter code}
+#'   \item{ANSI_digit}{American National Standards Institute 2-digit code. This is stored as a character to ensure all entries are two characters long.}
+#'   \item{USPS}{United States Postal Service 2-letter code}
+#'   \item{USCG}{United States Coast Guard 2-letter code (used as prefices for vessel numbers).}
+#'   \item{GPO}{US Government Printing Office abbreviations}
+#'   \item{AP}{Associated Press Stylebook abbreviations}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations}
+"US_states"
