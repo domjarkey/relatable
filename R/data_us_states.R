d73fbcaaeb4a2d36b3f8facc2d368ad4c1156929
Dominic Jarkey <dominic.jarkey@gmail.com>
2018-01-31 15:53:26 -0600

    Separate data code for elements and US_states

--- R/data_us_states.R
+++ R/data_us_states.R
@@ -1,16 +1,3 @@
-#' Data from the periodic table of elements
-#'
-#' A dataset containing atomic numbers, chemical symbols, and
-#' names of 118 elements.
-#'
-#' @format A data frame with 118 rows and 3 variables:
-#' \describe{
-#'   \item{Z}{Atomic number}
-#'   \item{Symbol}{Chemical symbol}
-#'   \item{Name}{Name of element}
-#' }
-#' @source \url{https://en.wikipedia.org/wiki/Symbol_(chemistry)}
-"elements"
 #' Data of US state names and abbreviations
 #'
 #' A dataset containing names of US states and their abbreviations under various standards
